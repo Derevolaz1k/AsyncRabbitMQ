c0bcd6e9663b6657bb6ea5733d67e2a447143114
Derevolaz1k <0_the_kick_0@mail.ru>
2023-10-25 19:57:39 +0300

    Initial commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "first",
+    exclusive: false,
+    durable: true,
+    autoDelete: false,
+    arguments: null);
+
+channel.QueueDeclare(
+    queue: "second",
+    exclusive: false,
+    durable: true,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received in first: {message}");
+
+    var responseMessage = "Сообщение доставлено.";
+    var responseBody = Encoding.UTF8.GetBytes(responseMessage);
+    Thread.Sleep(10000);
+    channel.BasicPublish(exchange: "", routingKey: "second", basicProperties: null, body: responseBody);
+};
+
+channel.BasicConsume(queue: "first", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Нажмите [Enter], чтобы выйти.");
+Console.ReadLine();
---
+++ Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+    channel.QueueDeclare(
+        queue: "first",
+        exclusive: false,
+        durable: true,
+        autoDelete: false,
+        arguments: null);
+
+channel.QueueDeclare(
+    queue: "second",
+    exclusive: false,
+    durable: true,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received in second: {message}");
+};
+
+channel.BasicConsume(queue: "second", autoAck: true, consumer: consumer);
+
+while (true)
+{
+    Console.WriteLine("Введите сообщение:");
+    var message = Console.ReadLine();
+    var body = Encoding.UTF8.GetBytes(message);
+        
+    channel.BasicPublish(exchange: "", routingKey: "first", basicProperties: null, body: body);
+}
